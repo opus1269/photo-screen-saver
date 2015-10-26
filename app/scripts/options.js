49d403a82b76d14e7a4806ec6ef5547673b96cf3
opus1269 <igreilly1269@gmail.com>
2015-10-26 11:01:36 -0500

    chnage template id

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -7,56 +7,66 @@
 	// Grab a reference to our auto-binding template
 	// and give it some initial binding values
 	// Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-	var app = document.querySelector('#app');
+	var t = document.querySelector('#t');
 
 	// grap a few other convenient references
-	app.menu;
-	app.errorDialog;
-	app.albumT;
+	t.menu;
+	t.errorDialog;
+	t.albumT;
 
 	// Dialog for error messages
-	app.errorDialog;
-	app.dialogText = '';
+	t.errorDialog;
+	t.dialogText = '';
 
 	// current and previous route
 	// several menu items open a new tab or window and we
 	// need to keep the selected menu item and the current page in sync
-	app.route = 'page-settings';
-	app.prevRoute = 'page-settings';
+	t.route = 'page-settings';
+	t.prevRoute = 'page-settings';
 
 	// display spinner while performing lengthy operations
-	app.waitForLoad = false;
+	t.waitForLoad = false;
 
 	// array of Google Photos Albums
-	app.albums = [];
+	t.albums = [];
+
+	// array photo sizing types
+	t.photoSizingItems = ['Letterbox','Fill Screen','Center &amp; Frame'];
+
+	// array photo transition types
+	t.photoTransitionItems = ['Scale up','Fade','Slide from right','Slide down','Spin up'];
+
+	// array of Flickr search terms
+	t.flickrSearchItems = ['None', 'Interesting - daily', 'Favorites'];
 
 	// array of selected google albums
-	app.albumSelections = JSON.parse(localStorage.albumSelections);
+	t.albumSelections = JSON.parse(localStorage.albumSelections);
 
 	// Listen for template bound event to know when bindings
 	// have resolved and content has been stamped to the page
-	app.addEventListener('dom-change', function() {
-		app.menu = app.$.mainMenu;
-		app.errorDialog = app.$.errorDialog;
-		app.albumT = app.$.albumsTemplate;
-		console.log('dom-change', app.errorDialog, app.menu, app.albumT);
+	t.addEventListener('dom-change', function() {
+		t.menu = t.$.mainMenu;
+		t.errorDialog = t.$.errorDialog;
+		t.albumT = t.$.albumsTemplate;
+		console.log('dom-change', t.errorDialog, t.menu, t.albumT);
+
 	});
 
 	// See https://github.com/Polymer/polymer/issues/1381
 	// window.addEventListener('WebComponentsReady', function() {
 	//   // imports are loaded and elements have been registered
-	//   app.menu = Polymer.dom(document).querySelector('#mainMenu');
-	//   app.errorDialog = Polymer.dom(document).querySelector('#errorDialog');
-	//   console.log('WebComponentsReady', app.errorDialog, app.menu);
+	//   t.menu = Polymer.dom(document).querySelector('#mainMenu');
+	//   t.errorDialog = Polymer.dom(document).querySelector('#errorDialog');
+	//   console.log('WebComponentsReady', t.errorDialog, t.menu);
 	// });
 
 	// Scroll page to top
-	app.scrollPageToTop = function() {
+	t.scrollPageToTop = function() {
 		document.getElementById('mainContainer').scrollTop = 0;
 	};
 
 	// Close drawer if drawerPanel is narrow
-	app.closeDrawer = function() {
+	t.closeDrawer = function() {
 		var drawerPanel = document.querySelector('#paperDrawerPanel');
 		if (drawerPanel.narrow) {
 			drawerPanel.closeDrawer();
@@ -64,34 +74,34 @@
 	};
 
 	// handle menu selection of pages
-	app.onDataRouteClick = function(event) {
+	t.onDataRouteClick = function(event) {
 
 		// Close drawer after menu item is selected if drawerPanel is narrow
-		app.closeDrawer();
+		t.closeDrawer();
 
-		var index = app.pages.map(function(e) {return e.route;}).indexOf(event.srcElement.id);
+		var index = t.pages.map(function(e) {return e.route;}).indexOf(event.srcElement.id);
 
-		app.prevRoute = app.route;
+		t.prevRoute = t.route;
 
-		if (Boolean(app.pages[index].obj)) {
-			if (typeof app.pages[index].obj === 'string') {
+		if (Boolean(t.pages[index].obj)) {
+			if (typeof t.pages[index].obj === 'string') {
 				// some pages are url links
-				app.menu.select(app.prevRoute);
-				chrome.tabs.create({url: app.pages[index].obj});
+				t.menu.select(t.prevRoute);
+				chrome.tabs.create({url: t.pages[index].obj});
 			} else {
-				app.pages[index].obj(index,event);
+				t.pages[index].obj(index,event);
 			}
 		} else {
-			app.route = app.pages[index].route;
+			t.route = t.pages[index].route;
 			this.debounce('job1', function() {
-				app.scrollPageToTop();
+				t.scrollPageToTop();
 				this.fire('done');
 			}, 500);
 		}
 	};
 
 	// Refresh the list of albums
-	app.refreshAlbums = function(items) {
+	t.refreshAlbums = function(items) {
 		var tmp = [];
 		var item;
 
@@ -102,19 +112,19 @@
 				name: item.name
 			});
 		}
-		app.albums = tmp;
+		t.albums = tmp;
 	};
 
 	// show the Google Photos page
-	app.googlePhotos = function(index) {
-		app.route = app.pages[index].route;
-		app.loadAlbumList();
+	t.googlePhotos = function(index) {
+		t.route = t.pages[index].route;
+		t.loadAlbumList();
 	};
 
 	// preview the screensaver
-	app.preview = function() {
-		app.async(function() {
-			app.menu.select(app.prevRoute);
+	t.preview = function() {
+		t.async(function() {
+			t.menu.select(t.prevRoute);
 		}, 500);
 		// localStorage.isPreview = 'true';
 		// chrome.runtime.getBackgroundPage(function(win) {
@@ -124,39 +134,39 @@
 	};
 
 	// list of pages
-	app.pages = [
+	t.pages = [
 		{label: 'Settings', route: 'page-settings', icon: 'settings', obj: null},
-		{label: 'Google Photos Albums', route: 'page-google-photos', icon: 'cloud', obj: app.googlePhotos},
-		{label: 'Preview (click to close)', route: 'page-preview', icon: 'pageview', obj: app.preview},
+		{label: 'Google Photos Albums', route: 'page-google-photos', icon: 'cloud', obj: t.googlePhotos},
+		{label: 'Preview (click to close)', route: 'page-preview', icon: 'pageview', obj: t.preview},
 		{label: 'Information For Nerds', route: 'page-info', icon: 'info', obj: null},
 		{label: 'Request Support', route: 'page-support', icon: 'help', obj: EXT_URI + 'support'},
 		{label: 'Rate Extension', route: 'page-rate', icon: 'grade', obj: EXT_URI + 'reviews'}
 	];
 
 	// Query Picasa for the list of the users albums
-	app.loadAlbumList = function() {
-		app.waitForLoad = true;
+	t.loadAlbumList = function() {
+		t.waitForLoad = true;
 		gPhotos.loadAlbumList(function(albums, error) {
 			//error = 'This is an eror';
 			if (!error) {
-				app.albums = albums;
-				app.selectAlbums();
+				t.albums = albums;
+				t.selectAlbums();
 			} else {
-				app.dialogText = error;
-				app.errorDialog.open();
+				t.dialogText = error;
+				t.errorDialog.open();
 			}
-			app.waitForLoad = false;
+			t.waitForLoad = false;
 		});
 	};
 
 	// set the checked state of the albums (before binding)
-	app.selectAlbums = function() {
+	t.selectAlbums = function() {
 		var i, j;
 
-		for (i = 0; i < app.albums.length; i++) {
-			for (j = 0; j < app.albumSelections.length; j++) {
-				if (app.albums[i].id === app.albumSelections[j].id) {
-					app.albums[i].checked = true;
+		for (i = 0; i < t.albums.length; i++) {
+			for (j = 0; j < t.albumSelections.length; j++) {
+				if (t.albums[i].id === t.albumSelections[j].id) {
+					t.albums[i].checked = true;
 					break;
 				}
 			}
@@ -164,38 +174,38 @@
 	};
 
 	// refresh the album list
-	app.albumRefreshTapped = function() {
-		app.loadAlbumList();
+	t.albumRefreshTapped = function() {
+		t.loadAlbumList();
 	};
 
 	// deselect all albums
-	app.albumsDeselectAllTapped = function() {
+	t.albumsDeselectAllTapped = function() {
 		var model;
 
-		for (var i = 0; i < app.albums.length; i++) {
-			if (app.albums[i].checked) {
-				model = app.albumT.modelForElement(document.querySelector('#' + app.albums[i].uid));
+		for (var i = 0; i < t.albums.length; i++) {
+			if (t.albums[i].checked) {
+				model = t.albumT.modelForElement(document.querySelector('#' + t.albums[i].uid));
 				model.set('item.checked', false);
 			}
 		}
-		app.albumSelections = [];
-		localStorage.albumSelections = JSON.stringify(app.albumSelections);
+		t.albumSelections = [];
+		localStorage.albumSelections = JSON.stringify(t.albumSelections);
 	};
 
 	// handle interactive album selections
-	app.albumSelectChange = function(event) {
+	t.albumSelectChange = function(event) {
 		var item = event.model.item;
-		var index = app.albumSelections.map(function(e) {return e.id;}).indexOf(item.id);
+		var index = t.albumSelections.map(function(e) {return e.id;}).indexOf(item.id);
 
 		if (index !== -1) {
-			app.albumSelections.splice(index, 1);
+			t.albumSelections.splice(index, 1);
 		}
 
 		if (item.checked) {
-			app.albumSelections.push({id: item.id, photos: item.photos});
+			t.albumSelections.push({id: item.id, photos: item.photos});
 		}
 
-		localStorage.albumSelections = JSON.stringify(app.albumSelections);
+		localStorage.albumSelections = JSON.stringify(t.albumSelections);
 	};
 
 })(document);
