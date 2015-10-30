05ea53e124ca4a6e196ba077d7df986f03e0bfe2
opus1269 <igreilly1269@gmail.com>
2015-10-29 22:51:02 -0500

    use elements for all the pages

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -11,12 +11,7 @@
 
 	// grap a few other convenient references
 	t.menu;
-	t.errorDialog;
-	t.albumT;
-
-	// Dialog for error messages
-	t.errorDialog;
-	t.dialogText = '';
+	t.gPhotosPage;
 
 	// current and previous route
 	// several menu items open a new tab or window and we
@@ -24,32 +19,12 @@
 	t.route = 'page-settings';
 	t.prevRoute = 'page-settings';
 
-	// display spinner while performing lengthy operations
-	t.waitForLoad = false;
-
-	// array of Google Photos Albums
-	t.albums = [];
-
-	// array photo sizing types
-	t.photoSizingItems = ['Letterbox','Fill screen','Frame'];
-
-	// array photo transition types
-	t.photoTransitionItems = ['Scale up','Fade','Slide from right','Slide down','Spin up'];
-
-	// array of time format options
-	t.timeItems = ['No', '12 hour format', '24 hour format'];
-
-	// array of selected google albums
-	t.albumSelections = JSON.parse(localStorage.albumSelections);
-
 	// Listen for template bound event to know when bindings
 	// have resolved and content has been stamped to the page
 	t.addEventListener('dom-change', function() {
 		t.menu = t.$.mainMenu;
-		t.errorDialog = t.$.errorDialog;
-		t.albumT = t.$.albumsTemplate;
-		console.log('dom-change', t.errorDialog, t.menu, t.albumT);
-
+		t.gPhotosPage = t.$.gPhotosPage;
+		console.log('dom-change', t.menu, t.gPhotosPage);
 	});
 
 	// See https://github.com/Polymer/polymer/issues/1381
@@ -60,20 +35,7 @@
 	//   console.log('WebComponentsReady', t.errorDialog, t.menu);
 	// });
 
-	// Scroll page to top
-	t.scrollPageToTop = function() {
-		document.getElementById('scrollPanel').scrollToTop();
-	};
-
-	// Close drawer if drawerPanel is narrow
-	t.closeDrawer = function() {
-		var drawerPanel = document.querySelector('#paperDrawerPanel');
-		if (drawerPanel.narrow) {
-			drawerPanel.closeDrawer();
-		}
-	};
-
-	// handle menu selection of pages
+	// handle main menu selections
 	t.onDataRouteClick = function(event) {
 
 		// Close drawer after menu item is selected if drawerPanel is narrow
@@ -100,25 +62,10 @@
 		}
 	};
 
-	// Refresh the list of albums
-	t.refreshAlbums = function(items) {
-		var tmp = [];
-		var item;
-
-		for (var i = 0; i < items.length; i++) {
-			item = items[i];
-			tmp.push({
-				checked: item.checked,
-				name: item.name
-			});
-		}
-		t.albums = tmp;
-	};
-
 	// show the Google Photos page
 	t.googlePhotos = function(index) {
 		t.route = t.pages[index].route;
-		t.loadAlbumList();
+		t.gPhotosPage.loadAlbumList();
 	};
 
 	// preview the screensaver
@@ -139,69 +86,17 @@
 		{label: 'Rate Extension', route: 'page-rate', icon: 'grade', obj: EXT_URI + 'reviews'}
 	];
 
-	// Query Picasa for the list of the users albums
-	t.loadAlbumList = function() {
-		t.waitForLoad = true;
-		gPhotos.loadAlbumList(function(albums, error) {
-			//error = 'This is an eror';
-			if (!error) {
-				t.albums = albums;
-				t.selectAlbums();
-			} else {
-				t.dialogText = error;
-				t.errorDialog.open();
-			}
-			t.waitForLoad = false;
-		});
+	// Scroll page to top
+	t.scrollPageToTop = function() {
+		document.getElementById('scrollPanel').scrollToTop();
 	};
 
-	// set the checked state of the albums (before binding)
-	t.selectAlbums = function() {
-		var i, j;
-
-		for (i = 0; i < t.albums.length; i++) {
-			for (j = 0; j < t.albumSelections.length; j++) {
-				if (t.albums[i].id === t.albumSelections[j].id) {
-					t.albums[i].checked = true;
-					break;
-				}
-			}
+	// Close drawer if drawerPanel is narrow
+	t.closeDrawer = function() {
+		var drawerPanel = document.querySelector('#paperDrawerPanel');
+		if (drawerPanel.narrow) {
+			drawerPanel.closeDrawer();
 		}
-	};
-
-	// refresh the album list
-	t.albumRefreshTapped = function() {
-		t.loadAlbumList();
-	};
-
-	// deselect all albums
-	t.albumsDeselectAllTapped = function() {
-		var model;
-
-		for (var i = 0; i < t.albums.length; i++) {
-			if (t.albums[i].checked) {
-				model = t.albumT.modelForElement(document.querySelector('#' + t.albums[i].uid));
-				model.set('item.checked', false);
-			}
-		}
-		t.albumSelections = [];
-		localStorage.albumSelections = JSON.stringify(t.albumSelections);
-	};
-
-	// handle interactive album selections
-	t.albumSelectChange = function(event) {
-		var item = event.model.item;
-		var index = t.albumSelections.map(function(e) {return e.id;}).indexOf(item.id);
-
-		if (index !== -1) {
-			t.albumSelections.splice(index, 1);
-		}
-
-		if (item.checked) {
-			t.albumSelections.push({id: item.id, photos: item.photos});
-		}
-
-		localStorage.albumSelections = JSON.stringify(t.albumSelections);
 	};
 
 })(document);
