066f074e0cadc488c0e255d3361b8b7717e62e0a
opus1269 <igreilly1269@gmail.com>
2017-05-13 03:25:59 -0500

    move storage handling to own file

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -75,102 +75,12 @@ app.Utils = (function() {
 		},
 
 		/**
-		 * Get integer value from localStorage
-		 * @param {!string} key - key to get value for
-		 * @returns {?int} value as integer
-		 * @memberOf app.Utils
-		 */
-		getInt: function(key) {
-			let item = localStorage.getItem(key);
-			if (item !== null) {
-				item = parseInt(item, 10);
-			}
-			return item;
-		},
-
-		/**
-		 * Get boolean value from localStorage
-		 * @param {!string} key - key to get value for
-		 * @returns {?boolean} value as boolean
-		 * @memberOf app.Utils
-		 */
-		getBool: function(key) {
-			return app.Utils.get(key);
-		},
-
-		/**
-		 * Get a JSON parsed value from localStorage
-		 * @param {!string} key - key to get value for
-		 * @returns {?JSON} JSON object, null if key does not exist
-		 * @memberOf app.Utils
-		 */
-		get: function(key) {
-			let item = localStorage.getItem(key);
-			if (item !== null) {
-				item = JSON.parse(item);
-			}
-			return item;
-		},
-
-		/**
-		 * JSON stringify and save a value to localStorage
-		 * @param {!string} key - key to set value for
-		 * @param {?Object} value - new value, if null remove item
-		 * @memberOf app.Utils
-		 */
-		set: function(key, value) {
-			if (value !== null) {
-				localStorage.setItem(key, JSON.stringify(value));
-			} else {
-				localStorage.removeItem(key);
-			}
-		},
-
-		/**
-		 * Save a value to localStorage only if there is enough room
-		 * @param {!string} key - localStorage Key
-		 * @param {Object} value - value to save
-		 * @param {string} [keyBool] - key to a boolean value
-		 *                 that is true if the primary key has non-empty value
-		 * @returns {boolean} true if value was set successfully
-		 * @memberOf app.Utils
-		 */
-		safeSet: function(key, value, keyBool) {
-			let ret = true;
-			const oldValue = app.Utils.get(key);
-			try {
-				app.Utils.set(key, value);
-			} catch (e) {
-				ret = false;
-				if (oldValue) {
-					// revert to old value
-					app.Utils.set(key, oldValue);
-				}
-				if (keyBool) {
-					// revert to old value
-					if (oldValue && oldValue.length) {
-						app.Utils.set(keyBool, true);
-					} else {
-						app.Utils.set(keyBool, false);
-					}
-				}
-				// notify listeners
-				chrome.runtime.sendMessage({
-					message: 'storageExceeded',
-					name: keyBool,
-				}, function(response) {});
-			}
-
-			return ret;
-		},
-
-		/**
 		 * Get the idle time in seconds
 		 * @returns {int} idle time in seconds
 		 * @memberOf app.Utils
 		 */
 		getIdleSeconds: function() {
-			const idle = app.Utils.get('idleTime');
+			const idle = app.Storage.get('idleTime');
 			return idle.base * 60;
 		},
 
@@ -180,7 +90,7 @@ app.Utils = (function() {
 		 * @memberOf app.Utils
 		 */
 		isWin: function() {
-			return app.Utils.get('os') === 'win';
+			return app.Storage.get('os') === 'win';
 		},
 
 		/**
